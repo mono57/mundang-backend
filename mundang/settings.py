7e024dfff8230522ebaf83c316a8abc5383dfebf
Amono Aymar <aymar.amono@phoenix-gs.xyz>
2022-03-15 23:47:50 +0100

    fix: update deployment settings

--- mundang/settings.py
+++ mundang/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r=z9egj4nhw7j6&u(0p5h66v$s-sa+%g-0y=i527$lza@hod1+'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-r=z9egj4nhw7j6&u(0p5h66v$s-sa+%g-0y=i527$lza@hod1+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
