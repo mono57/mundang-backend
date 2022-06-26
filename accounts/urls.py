bc86988997ededaa0fe8d59ee7feec6ade786221
Aymar Amono <aymaramono@gmail.com>
2022-06-26 17:56:16 +0100

    refactor(accounts):  update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts.views import SignupView, VerifyInviteView, DashboardTemplateView
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', SignupView.as_view(), name='signup'),
+    path('signup/', SignupView.as_view(), name='signup'),
     path('invite/<str:referral_code>/verify', VerifyInviteView.as_view(), name='user_invite'),
     path('dashboard/', DashboardTemplateView.as_view(), name='dashboard'),
 ]
